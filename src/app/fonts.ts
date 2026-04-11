f6cf40ea640f5f7e79aa5fb42692c20ececf5963
Harshal Patel <hp842484@gmail.com>
2026-04-11 20:31:39 +0530

    fix(fonts): final native framework-level font resolution using next/font/local and ClassNames

---
+++ src/app/fonts.ts
@@ -0,0 +1,17 @@
+import localFont from 'next/font/local'
+
+export const cirka = localFont({
+  src: './fonts/Cirka-Bold.otf',
+  variable: '--font-cirka',
+  weight: '700',
+  style: 'normal',
+  display: 'swap',
+})
+
+export const theSeason = localFont({
+  src: './fonts/SeasonSans-TRIAL-Regular.otf',
+  variable: '--font-season',
+  weight: '400',
+  style: 'normal',
+  display: 'swap',
+})
